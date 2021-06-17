655f33f43ca3c9bcf52e74fa059a1ac26cd06d13
Rostom Mathlouthi <mathlouthi.rostom@gmail.com>
2021-06-17 23:51:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [person, setperson] = useState([])
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+                setperson(res.data)
+            }
+            catch (error) {
+                console.log(error.response.data)
+            }
+            
+        }
+        getData()
+        
+    }, [])
+    return (
+        <div>
+            <h2>User List</h2>
+        <div className="App">
+           
+
+            {person.map((user, i) => {
+                return (
+
+                    <div key={user.id} className="person">
+                        <ul className="list">
+                        <li><span>Name:</span> {user.name}</li>
+                        <li><span>Username:</span> {user.username}</li>
+                        <li><span>Email:</span> {user.email}</li>
+                            <li><span>Phone:</span>{user.phone}</li>
+                        </ul>    
+                    </div>
+                )
+            })}
+            </div>
+            </div>
+    )
 }
 
-export default App;
+export default App
+
